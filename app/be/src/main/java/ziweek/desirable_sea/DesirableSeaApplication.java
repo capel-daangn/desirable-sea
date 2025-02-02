10a660181e4bb930d1cb2c3f073d55c8fc5ef978
ziweek <alex.jiuk.kim@gmail.com>
2025-02-03 08:37:06 +0900

    .

---
+++ app/be/src/main/java/ziweek/desirable_sea/DesirableSeaApplication.java
@@ -0,0 +1,13 @@
+package ziweek.desirable_sea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesirableSeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesirableSeaApplication.class, args);
+	}
+
+}
---
+++ app/be/src/test/java/ziweek/desirable_sea/DesirableSeaApplicationTests.java
@@ -0,0 +1,13 @@
+package ziweek.desirable_sea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesirableSeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
